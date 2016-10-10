06809c8037bfde23f5f37eb410e8468b994d0d29
UserPL022Pc16 <UserPL022Pc16@SLFRPL022PC16.ensino.atec.local>
2016-10-10 08:13:14 +0100

    Segundo

--- src/exercicios071016/Treino.java
+++ src/exercicios071016/Treino.java
@@ -24,6 +24,28 @@ public class Treino {
 				System.out.print("O numero 2 e o maior");
 			}
 		}
+		
+		private void exercicio2() {
+			
+			System.out.print("Introduza o numero 1 ");
+			int num1 = (new Scanner(System.in)).nextInt();
+			System.out.print("Introduza o numero 2 ");
+			int num2 = (new Scanner(System.in)).nextInt();
+			System.out.print("Introduza o numero 3 ");
+			int num3 = (new Scanner(System.in)).nextInt();
+			
+			if (num1 > num2 && num1 >num3){
+				
+				System.out.print("O numero 1 e o maior ");
+			}
+			
+			else if (num2 > num1 && num2 > num3){
+				System.out.print("O numero 1 e o maior ");	
+		}
+		
+		else
+			System.out.print("O numero 3 e o maior ");
+		
 	
 
-}
+}}
