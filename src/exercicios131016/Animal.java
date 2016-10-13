47734a628d9aaa59b70e4ac6ae0230444e0d9367
UserPL022Pc16 <UserPL022Pc16@SLFRPL022PC16.ensino.atec.local>
2016-10-13 15:00:23 +0100

    lojaAnimais

---
+++ src/exercicios131016/Animal.java
@@ -0,0 +1,67 @@
+package exercicios131016;
+
+public class Animal {
+
+	String tipo, raca, cor;
+	int idadeAno, idadeMes;
+	double peso;
+
+	public Animal(String tipo, String raca, String cor, int idadeAno, int idadeMes, double peso) {
+		super();
+		this.tipo = tipo;
+		this.raca = raca;
+		this.cor = cor;
+		this.idadeAno = idadeAno;
+		this.idadeMes = idadeMes;
+		this.peso = peso;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public int getIdadeAno() {
+		return idadeAno;
+	}
+
+	public void setIdadeAno(int idadeAno) {
+		this.idadeAno = idadeAno;
+	}
+
+	public int getIdadeMes() {
+		return idadeMes;
+	}
+
+	public void setIdadeMes(int idadeMes) {
+		this.idadeMes = idadeMes;
+	}
+
+}
---
+++ src/exercicios131016/Materiais.java
@@ -0,0 +1,57 @@
+package exercicios131016;
+
+public class Materiais {
+
+	String tipo, classe, cor, marca;
+	double peso;
+	
+	public Materiais(String tipo, String classe, String cor, String marca, double peso) {
+		super();
+		this.tipo = tipo;
+		this.classe = classe;
+		this.cor = cor;
+		this.marca = marca;
+		this.peso = peso;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getClasse() {
+		return classe;
+	}
+
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+}
